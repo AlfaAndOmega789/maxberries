fcb5255a0292836effe7ab22675ce00b5f5311d6
ivan <ivan.koval.andr@gmail.com>
2025-10-30 16:49:07 +0300

    Добавил интерфейсы в usecase для product и category

--- catalog_service/internal/domain/usecase/category_usecase.go
+++ catalog_service/internal/domain/usecase/category_usecase.go
@@ -1,1 +1,10 @@
 package usecase
+
+import "maxberries/catalog_service/internal/domain/entity"
+
+type CategoryUsecase interface {
+	GetAll() ([]entity.Category, error)
+	Create(p entity.Category, err error)
+	Update(id int, p entity.Category) error
+	Delete(id int) error
+}
